38b1849b2d83f6822f1703b7fc8d9d15994514a2
Cristian Navarro <95457181+SoyCristiann@users.noreply.github.com>
2025-08-27 22:55:22 -0500

    Código original

---
+++ CalcularAreas/src/AreaCalculator.java
@@ -0,0 +1,16 @@
+public class AreaCalculator {
+    public double sum(Object[] shapes) {
+        double totalArea = 0;
+        for (Object shape : shapes) {
+            if (shape instanceof Rectangle) {
+                Rectangle rect = (Rectangle) shape;
+                totalArea += rect.getWidth() * rect.getHeight();
+            }
+            if (shape instanceof Circle) {
+                Circle circle = (Circle) shape;
+                totalArea += Math.PI * circle.getRadius() * circle.getRadius();
+            }
+        }
+        return totalArea;
+    }
+}
